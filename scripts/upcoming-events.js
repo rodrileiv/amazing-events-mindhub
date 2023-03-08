0d189e90e252484282ab274e3638d95dd64b8284
rodri <mig_45_45_45@hotmail.com>
2023-03-08 12:45:44 -0300

    v 3.3

--- scripts/upcoming-events.js
+++ scripts/upcoming-events.js
@@ -12,45 +12,93 @@ return upcomingEvents;
 }
 console.log(futureDate(data.events));
 
-let cardsHome = document.getElementById("cards-home");
+/* --------------------------------------- */
+let events = data.events;
+
 let fragment = document.createDocumentFragment();
 
-for (events of upcomingEvents){
-    let title = document.createElement('h3');
-    title.className = "titleCard";
-    title.textContent = events.name;
-    fragment.appendChild(title);
+function createCard(arr, contenedor) {
+    let cardsHome = document.querySelector(contenedor)
+    cardsHome.innerHTML = ""
+    for (events of arr) {
+        let title = document.createElement('h3');
+        title.className = "titleCard";
+        title.textContent = events.name;
+        fragment.appendChild(title);
 
-    let priceP = document.createElement('p');
-    priceP.className = "priceP";
-    priceP.textContent = "Price: $" + events.price;
+        let priceP = document.createElement('p');
+        priceP.className = "priceP";
+        priceP.textContent = "Price: $" + events.price;
 
-    let image = document.createElement('img');
-    image.className = "imageCard";
-    image.src = events.image;
-    fragment.appendChild(image);
+        let image = document.createElement('img');
+        image.className = "imageCard";
+        image.src = events.image;
+        fragment.appendChild(image);
 
-    let viewMore = document.createElement('a');
-    viewMore.className = "viewMore";
-    viewMore.textContent = "View more...";
-    viewMore.href = "./details.html";
+        let viewMore = document.createElement('a');
+        viewMore.className = "viewMore";
+        viewMore.innerHTML = `
+        <a href="./details.html?id=${events._id}">View more...</a>`
 
-    let divPrice = document.createElement('div');
-    divPrice.className = "price";
-    divPrice.appendChild(priceP);
-    divPrice.appendChild(viewMore);
+        let divPrice = document.createElement('div');
+        divPrice.className = "price";
+        divPrice.appendChild(priceP);
+        divPrice.appendChild(viewMore);
 
-    let paragraph = document.createElement('p');
-    paragraph.className = "pCard";
-    paragraph.textContent = events.description;
-    fragment.appendChild(paragraph);
+        let paragraph = document.createElement('p');
+        paragraph.className = "pCard";
+        paragraph.textContent = events.description;
+        fragment.appendChild(paragraph);
 
-    let divContainer = document.createElement('div');
-    divContainer.className = "container-cards-home";
-    divContainer.appendChild(title);
-    divContainer.appendChild(image);
-    divContainer.appendChild(paragraph);
-    divContainer.appendChild(divPrice);
-    fragment.appendChild(divContainer);
+        let divContainer = document.createElement('div');
+        divContainer.className = "container-cards-home";
+        divContainer.appendChild(title);
+        divContainer.appendChild(image);
+        divContainer.appendChild(paragraph);
+        divContainer.appendChild(divPrice);
+        fragment.appendChild(divContainer);
+    };
+    cardsHome.appendChild(fragment);
+}
+createCard(upcomingEvents, "#cards-home")
+
+/* CATEGORIAS DOM */
+
+let categories = [];
+
+let category = document.getElementById("form-category")
+data.events.forEach(e => {
+    if (!categories.includes(e.category)) {
+    categories.push(e.category)
+    category.innerHTML += `
+        <div id="content-cat">
+            <label class="checkbox">
+                <input type="checkbox" name="category" id="${e.category}" value="${e.category}">
+                <span>${e.category}</span>
+            </label>
+        </div>`
+}
+});
+
+/* FILTRO CATEGORÍA */
+
+let checkButton = document.querySelectorAll("input[type='checkbox']")
+let eventsChecked = []
+
+checkButton.forEach(boton => boton.addEventListener('change', check))
+function check() {
+    eventsChecked = []
+    let select = Array.from(checkButton).filter(e => e.checked)
+    for (const event of upcomingEvents) {
+        select.forEach(input => {
+        if (event.category == input.value) {
+            eventsChecked.push(event)
+        }
+    });
+}
+    if (eventsChecked.length > 0) {
+        createCard(eventsChecked, "#cards-home")
+    } else {
+        createCard(upcomingEvents, "#cards-home")
+    }
 };
-cardsHome.appendChild(fragment);
