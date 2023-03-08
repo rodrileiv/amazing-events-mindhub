f5157e8e1c155971d58cf8edf76e29bb2c1cf1b9
rodri <mig_45_45_45@hotmail.com>
2023-03-08 20:35:47 -0300

    v 3.4

---
+++ scripts/details.js
@@ -0,0 +1,27 @@
+let query = location.search
+
+let param = new URLSearchParams(query)
+
+let id = param.get("id")
+
+let events = data.events
+
+let eventDetail = events.find(eventDetail => eventDetail._id == id)
+
+let buildDetails = document.querySelector(".cards-details")
+
+    buildDetails.innerHTML = `<div class="details">
+        <img src="${eventDetail.image}" alt="${eventDetail.name}">
+        <div class="details-text">
+            <h3>${eventDetail.name}</h3>
+            <p>${eventDetail.description}</p>
+            <div>
+            <p>Date: ${eventDetail.date}</p>
+            <p>Category: ${eventDetail.category}</p>
+            <p>Place: ${eventDetail.place}</p>
+            <p>Capacity: ${eventDetail.capacity}</p>
+            <p>Price: $${eventDetail.price}</p>
+            </div>
+        </div>
+    </div>`
+
